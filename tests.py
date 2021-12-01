17b5d33f51e94c63b76b0beb66802881d36f05b9
singhalyash8080 <you@example.com>
2021-12-01 19:51:26 +0530

    added all the tests

--- tests.py
+++ tests.py
@@ -62,17 +62,15 @@ class TestAppMethods(unittest.TestCase):
         
         self.base = sqlite3.connect("login.db")
         self.cur = self.base.cursor()
-
         
         self.assertEqual(User.getproducts(self,user='a'),1)
 
 
-    # Checking if on providing  product id we get product price
+    # Checking if on providing product id we get product price
     def test_product_price(self):
         
         self.base = sqlite3.connect("login.db")
         self.cur = self.base.cursor()
-
         
         
         self.assertEqual(User.clicktranstable(self,user='100730'),60)
@@ -86,12 +84,50 @@ class TestAppMethods(unittest.TestCase):
         self.assertEqual(1,1)
 
 
-    # def testAddItem(self):
-    #     self.base = sqlite3.connect("login.db")
-    #     self.cur = self.base.cursor()
-    #     Admin.AddItem(self,item_code='111111',item_name='Brush',item_desc='Colgate Toothbrush',item_cat='OTHERS',item_price='20',item_stocks='500')
+    def testAddItem(self):
+        self.base = sqlite3.connect("login.db")
+        self.cur = self.base.cursor()
+        res = Admin.AddItem(self,item_code='999998',item_name='Soap 2',item_desc='Lux Soap 2',item_cat='OTHERS',item_price='50',item_stocks='500')
 
-    #     self.assertEqual(1,1)
+        self.assertEqual(res,1)
+
+    def test_userUpdate(self):
+
+        self.base = sqlite3.connect("login.db")
+        self.cur = self.base.cursor()
+
+        username = 'YASH'
+        password = '123456'
+        accountType = 'USER'
+
+        res = Admin.changeusertable(self, username.upper(), password.upper(), accountType.upper())
+
+        # checking if the user is updated successfully
+        self.assertEqual(res, 1)
+
+    def test_userDelete(self):
+
+        self.base = sqlite3.connect("login.db")
+        self.cur = self.base.cursor()
+
+        username = 'YASH'
+
+        res = Admin.deluser(self, username.upper())
+
+        # checking if the user is deleted successfully
+        self.assertEqual(res, 1)
+
+    def test_deleteProduct(self):
+            
+        self.base = sqlite3.connect("login.db")
+        self.cur = self.base.cursor()
+    
+        productID = '1'
+
+        res = Admin.delproduct(self, productID)
+
+        # checking if the product is deleted successfully
+        self.assertEqual(res, 1)
 
 if __name__ == '__main__':
     unittest.main()
